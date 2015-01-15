85aa3241f3f37ef8d8f547b2026772d3258bae33
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-01-15 21:02:26 +0900

    syntax check

---
+++ spec/mock_spec.rb
@@ -0,0 +1,9 @@
+# require 'norikra/listener_spec_helper'
+
+# include Norikra::ListenerSpecHelper
+
+require 'norikra/listener/mock'
+
+describe Norikra::Listener::Mock do
+  it 'works well' # TODO: write!
+end
