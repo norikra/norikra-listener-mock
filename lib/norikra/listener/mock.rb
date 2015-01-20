1411a4165053f4b65a7c9c4bd4055cd0be2a45c9
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-01-20 23:12:32 +0900

    fix w/ new custom listener API

--- lib/norikra/listener/mock.rb
+++ lib/norikra/listener/mock.rb
@@ -4,23 +4,40 @@ require "json"
 module Norikra
   module Listener
     class Mock < Norikra::Listener::Base
-      def self.check(query_group_name)
-        query_group_name && query_group_name =~ /^MOCK\((\d+)\)$/ && $1
+      def self.label
+        "MOCK"
+      end
+
+      def initialize(argument, query_name, query_group)
+        super
+        @stdout = STDOUT
+      end
+
+      def process_sync(news, olds)
+        news.each do |event|
+          @stdout.puts "#{@query_name}\t#{@argument}\t" + JSON.dump(event)
+        end
+      end
+    end
+
+    class MockAsync < Norikra::Listener::Base
+      def self.label
+        "MOCK_ASYNC"
       end
 
       attr_accessor :stdout
 
-      def initialize(query_name, query_group, events_statistics)
+      def initialize(argument, query_name, query_group)
         super
         @stdout = STDOUT
-        @repeat = Mock.check(query_group).to_i
+        @repeat = @argument.to_i
       end
 
       def process_async(events) # [ [unixtime, {event} ], ...
         # write events to STDOUT specified times, in background
         events.each do |time, e|
           @repeat.times do |i|
-            @stdout.puts @query_name + "\t#{i + 1}\t#{Time.at(time)}\t" + JSON.dump(e)
+            @stdout.puts "#{@query_name}\t#{i + 1}\t#{Time.at(time)}\t" + JSON.dump(e)
           end
         end
       end
